37d464cf03f84cc0bb82f8331f56fdc8ecd9a285
Mahdi Nasser <mhnasser23@gmail.com>
2022-09-21 11:01:11 +0300

    change font

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="font-primary ">
+    <div className="font-secondary ">
       <Header></Header>
       <Game></Game>
     </div>
